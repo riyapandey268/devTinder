287161950bbe512b74c0733df2885bada79acd58
Riya Pandey <120082022+riyapandey268@users.noreply.github.com>
2025-08-12 16:35:38 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 const express  = require("express");
 
 const app = express();
+//order of routes matters
 
-app.use ( (req, res)=>{
-    res.send("Hello from server");
+app.get("/user", (req, res) =>{
+    res.send ("user data send successfully");
 })
+
+app.post ("/user", (req, res) =>{
+    res.send ("users data posted successfully");
+});
+
+app.delete("/user", (req, res) =>{
+    res.send ("users data deleted successfully")
+})
+app.use ( "/test", (req, res)=>{
+    res.send("This is test");
+})
+// app.use ( "/hello", (req, res)=>{
+//     res.send("Hello hello hello");
+// })
+// app.use ( "/hello/2", (req, res)=>{
+//     res.send("this is hello 2");
+// })
+// app.use ( "/riya", (req, res)=>{
+//     res.send("This is riya pandey");
+// })
+// app.use ( "/", (req, res)=>{
+//     res.send("Hello from server");
+// })
 app.listen(3000,  ()=>{
 
 console.log("Server is succesfully listning on port 3000")
