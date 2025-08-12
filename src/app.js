dbdaaac5e9c283ea5a111e559b9c457d2674c016
Riya Pandey <120082022+riyapandey268@users.noreply.github.com>
2025-08-12 11:42:00 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express  = require("express");
+
+const app = express();
+
+app.use ( (req, res)=>{
+    res.send("Hello from server");
+})
+app.listen(3000,  ()=>{
+
+console.log("Server is succesfully listning on port 3000")
+});
+
+
+
+
